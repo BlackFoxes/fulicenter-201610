788636480e6e6df2d99bf8ea206203df1e6ad219
耿小泰 <clawpo@foxmail.com>
2017-01-10 09:22:45 +0800

    add bean

---
+++ app/src/main/java/cn/ucai/fulicenter/model/bean/CategoryGroupBean.java
@@ -0,0 +1,42 @@
+package cn.ucai.fulicenter.model.bean;
+
+/**
+ * Created by clawpo on 2017/1/10.
+ */
+
+public class CategoryGroupBean {
+
+    /**
+     * id : 344
+     * name : 最IN
+     * imageUrl : muying/2.jpg
+     */
+
+    private int id;
+    private String name;
+    private String imageUrl;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
