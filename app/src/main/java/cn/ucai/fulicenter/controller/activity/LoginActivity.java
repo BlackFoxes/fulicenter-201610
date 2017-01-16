c1d7244197a73123d526982eb0278eb71fbd0ea2
耿小泰 <clawpo@foxmail.com>
2017-01-16 17:34:26 +0800

    用户登录流程

--- app/src/main/java/cn/ucai/fulicenter/controller/activity/LoginActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/activity/LoginActivity.java
@@ -1,19 +1,42 @@
 package cn.ucai.fulicenter.controller.activity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.application.FuLiCenterApplication;
+import cn.ucai.fulicenter.application.I;
+import cn.ucai.fulicenter.model.bean.Result;
+import cn.ucai.fulicenter.model.bean.User;
+import cn.ucai.fulicenter.model.net.IModelUser;
+import cn.ucai.fulicenter.model.net.ModelUser;
+import cn.ucai.fulicenter.model.net.OnCompleteListener;
+import cn.ucai.fulicenter.model.net.SharePrefrenceUtils;
+import cn.ucai.fulicenter.model.utils.CommonUtils;
+import cn.ucai.fulicenter.model.utils.L;
+import cn.ucai.fulicenter.model.utils.ResultUtils;
 import cn.ucai.fulicenter.view.MFGT;
 
 /**
  * Created by clawpo on 2017/1/16.
  */
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = LoginActivity.class.getSimpleName();
+    @BindView(R.id.username)
+    EditText mUsername;
+    @BindView(R.id.password)
+    EditText mPassword;
+
+    IModelUser model;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,12 +48,70 @@ public class LoginActivity extends AppCompatActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.backClickArea:
+                MFGT.finish(this);
                 break;
             case R.id.btn_login:
+                checkInput();
                 break;
             case R.id.btn_register:
                 MFGT.gotoRegister(this);
                 break;
         }
+    }
+
+    private void checkInput() {
+        String username = mUsername.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+        if(TextUtils.isEmpty(username)){
+            mUsername.setError(getString(R.string.user_name_connot_be_empty));
+            mUsername.requestFocus();
+        }else if(TextUtils.isEmpty(password)){
+            mPassword.setError(getString(R.string.password_connot_be_empty));
+            mPassword.requestFocus();
+        }else{
+            login(username,password);
+        }
+    }
+
+    private void login(String username, String password) {
+        final ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setMessage(getString(R.string.logining));
+        model = new ModelUser();
+        model.login(this, username, password, new OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                if (s!=null){
+                    Result result = ResultUtils.getResultFromJson(s, User.class);
+                    L.e(TAG,"result="+result);
+                    if(result!=null){
+                        if (result.isRetMsg()){
+                            User user = (User) result.getRetData();
+                            SharePrefrenceUtils.getInstance(LoginActivity.this).saveUser(user.getMuserName());
+                            FuLiCenterApplication.setUser(user);
+                            MFGT.finish(LoginActivity.this);
+                        }else{
+                            if (result.getRetCode() == I.MSG_LOGIN_UNKNOW_USER){
+                                CommonUtils.showLongToast(getString(R.string.login_fail_unknow_user));
+                            }
+                            if (result.getRetCode() == I.MSG_LOGIN_ERROR_PASSWORD){
+                                CommonUtils.showLongToast(getString(R.string.login_fail_error_password));
+                            }
+                        }
+                    }else{
+                        CommonUtils.showLongToast(getString(R.string.login_fail));
+                    }
+                }else{
+                    CommonUtils.showLongToast(getString(R.string.login_fail));
+                }
+                dialog.dismiss();
+            }
+
+            @Override
+            public void onError(String error) {
+                dialog.dismiss();
+                L.e(TAG,"error="+error);
+                CommonUtils.showLongToast(error);
+            }
+        });
     }
 }
