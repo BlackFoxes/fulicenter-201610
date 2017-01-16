0a6b4c5ef875d3a437b93124657a37169ef2495e
耿小泰 <clawpo@foxmail.com>
2017-01-16 15:06:56 +0800

    登录和注册的网络请求及实现

---
+++ app/src/main/java/cn/ucai/fulicenter/model/net/IModelUser.java
@@ -0,0 +1,12 @@
+package cn.ucai.fulicenter.model.net;
+
+import android.content.Context;
+
+/**
+ * Created by clawpo on 2017/1/11.
+ */
+
+public interface IModelUser {
+    void login(Context context, String username,String password, OnCompleteListener<String> listener);
+    void register(Context context, String username, String usernick, String password, OnCompleteListener<String> listener);
+}
