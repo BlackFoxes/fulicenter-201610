d6ce42a97c9529437e69e67772e1029ebb6b4228
耿小泰 <clawpo@foxmail.com>
2017-01-19 17:25:43 +0800

    添加购物车页面

---
+++ app/src/main/java/cn/ucai/fulicenter/controller/fragment/CartFragment.java
@@ -0,0 +1,137 @@
+package cn.ucai.fulicenter.controller.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.application.FuLiCenterApplication;
+import cn.ucai.fulicenter.application.I;
+import cn.ucai.fulicenter.controller.adapter.BoutiqueAdapter;
+import cn.ucai.fulicenter.model.bean.CartBean;
+import cn.ucai.fulicenter.model.bean.User;
+import cn.ucai.fulicenter.model.net.IModelUser;
+import cn.ucai.fulicenter.model.net.ModelUser;
+import cn.ucai.fulicenter.model.net.OnCompleteListener;
+import cn.ucai.fulicenter.model.utils.CommonUtils;
+import cn.ucai.fulicenter.model.utils.ConvertUtils;
+import cn.ucai.fulicenter.model.utils.L;
+import cn.ucai.fulicenter.view.SpaceItemDecoration;
+
+import static cn.ucai.fulicenter.R.id.rv;
+import static cn.ucai.fulicenter.R.id.srl;
+import static cn.ucai.fulicenter.R.id.tv_refresh;
+
+/**
+ * Created by clawpo on 2017/1/11.
+ */
+
+public class CartFragment extends Fragment {
+    private static final String TAG = CartFragment.class.getSimpleName();
+
+    @BindView(tv_refresh)
+    TextView mTvRefresh;
+    @BindView(rv)
+    RecyclerView mRv;
+    @BindView(srl)
+    SwipeRefreshLayout mSrl;
+
+    LinearLayoutManager gm;
+    BoutiqueAdapter mAdapter;
+    IModelUser model;
+    @BindView(R.id.tv_nothing)
+    TextView mTvNomore;
+    User user;
+
+    private void initData(final int action) {
+        if (user!=null){
+            model.getCart(getContext(), user.getMuserName(), new OnCompleteListener<CartBean[]>() {
+                @Override
+                public void onSuccess(CartBean[] result) {
+                    mSrl.setRefreshing(false);
+                    mTvRefresh.setVisibility(View.GONE);
+                    mSrl.setVisibility(View.VISIBLE);
+                    mTvNomore.setVisibility(View.GONE);
+                    if (result != null && result.length > 0) {
+                        ArrayList<CartBean> list = ConvertUtils.array2List(result);
+                        L.e(TAG,"list.size="+list.size());
+                        if (action == I.ACTION_DOWNLOAD || action == I.ACTION_PULL_DOWN) {
+//                            mAdapter.initData(list);
+                        } else {
+//                            mAdapter.addData(list);
+                        }
+                    } else {
+                        mSrl.setVisibility(View.GONE);
+                        mTvNomore.setVisibility(View.VISIBLE);
+                    }
+                }
+
+                @Override
+                public void onError(String error) {
+                    mSrl.setRefreshing(false);
+                    mTvRefresh.setVisibility(View.GONE);
+                    mSrl.setVisibility(View.GONE);
+                    mTvNomore.setVisibility(View.VISIBLE);
+                    CommonUtils.showShortToast(error);
+                    L.e(TAG, "error=" + error);
+                }
+            });
+        }
+    }
+
+    private void initView() {
+        mSrl.setColorSchemeColors(
+                getResources().getColor(R.color.google_blue),
+                getResources().getColor(R.color.google_green),
+                getResources().getColor(R.color.google_red),
+                getResources().getColor(R.color.google_yellow)
+        );
+        gm = new LinearLayoutManager(getContext());
+        mRv.addItemDecoration(new SpaceItemDecoration(12));
+        mRv.setLayoutManager(gm);
+        mRv.setHasFixedSize(true);
+        mAdapter = new BoutiqueAdapter(getContext(), null);
+        mRv.setAdapter(mAdapter);
+        mSrl.setVisibility(View.GONE);
+        mTvNomore.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.fragment_cart, container, false);
+        ButterKnife.bind(this, layout);
+        initView();
+        model = new ModelUser();
+        user = FuLiCenterApplication.getUser();
+        initData(I.ACTION_DOWNLOAD);
+        setPullDownListener();
+        return layout;
+    }
+
+    private void setPullDownListener() {
+        mSrl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mSrl.setRefreshing(true);
+                mTvRefresh.setVisibility(View.VISIBLE);
+                initData(I.ACTION_PULL_DOWN);
+            }
+        });
+    }
+
+    @OnClick(R.id.tv_nothing)
+    public void onClick() {
+        initData(I.ACTION_DOWNLOAD);
+    }
+}
